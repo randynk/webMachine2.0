97d1b12e26bd8b27431984752b1f21861f688af8
Randy NKAMWA <118863581+randynk@users.noreply.github.com>
2023-03-09 12:00:11 +0100

    Update load.jl

--- load.jl
+++ load.jl
@@ -8,8 +8,6 @@ prix = []
 prices = []
 precis = []
 precisions = []
-playlink = ""
-applink = ""
 infos = open("info","r")
 info = readlines(infos)
 close(infos)
@@ -27,7 +25,6 @@ for i in eachline(aLaUne)
     end
 end
 close(aLaUne)
-
 nbre_rub = parse(Int64, info[1])
 for i in rubric
     begin
@@ -114,14 +111,16 @@ whatsapp = info[5]
 telegram = info[6]
 messenger = info[7]
 vendeur = info[8]
+playlink = info[9]
+applink = info[10]
 pages = open("index.html","a")
-head = """\n<!DOCTYPE html>\n<html>\n<head>\n    <meta charset="utf-8">\n    <title>VShop #Powered by V.I.A.A</title>\n    <link rel="stylesheet" type="text/css" href="design.css">\n    <link rel="stylesheet" type="text/css" href="designed.css">\n    <meta name="viewport" content="width=device-width, initial-scale=1">\n    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">\n    <link rel="stylesheet" type="text/css" href="contacts.css">\n	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script> \n	<script src="contacts.js"></script>\n    <link rel="icon" href="vendeur.png">\n</head>\n<body>\n<header class="fix">\n    <table>\n        <tr>\n            <td><img class="log" src="vendeur.png"></td>\n            <td><div class="help">\n    				<div class="researchs">\n    				    <i class="fa fa-search" aria-hidden="true"></i>\n    				</div>\n    				<div class="entree">\n    				    <label id="search">Tapez votre recherche ici</label><br>\n    				    <input type="text" class="input"></input>\n    				</div>\n				</div>\n            </td>\n            <td class="hea"><div>\n"""
+head = """\n<!DOCTYPE html>\n<html>\n<head>\n    <meta charset="utf-8">\n    <title>$service #Powered by V.I.A.A</title>\n    <link rel="stylesheet" type="text/css" href="design.css">\n    <link rel="stylesheet" type="text/css" href="designed.css">\n    <meta name="viewport" content="width=device-width, initial-scale=1">\n    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">\n    <link rel="stylesheet" type="text/css" href="contacts.css">\n	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script> \n	<script src="contacts.js"></script>\n    <link rel="icon" href="vendeur.png">\n</head>\n<body>\n<header class="fix">\n    <table>\n        <tr>\n            <td><img class="log" src="vendeur.png"></td>\n            <td><div class="help">\n    				<div class="researchs">\n    				    <i class="fa fa-search" aria-hidden="true"></i>\n    				</div>\n    				<div class="entree">\n    				    <label id="search">Tapez votre recherche ici</label><br>\n    				    <input type="text" class="input"></input>\n    				</div>\n				</div>\n            </td>\n            <td class="hea"><div>\n"""
 for i in 1:nbre_rub
     global  head = string(head, """\n<button class="rubrique"><a href="index.html#rub-$i">$(rubrique[i])</a></button>\n""")
 end
 head = string(head, """\n				</div>\n</td>\n</tr>\n</table>\n</header>\n\n<span class="boutonhaut"><a href="#basdepage"><img src="bas.png"></a></span>\n	<div class="wrapper">\n		<div class="search-box">\n			<button class="inputs">\n				<a href="mailto:$email"><img src="vmail.png" class="clicks"></a>\n				<a href="https://wa.me/+237$whatsapp"><img src="whtsp.png" class="clicks"></a>\n				<a href="https://t.me/+237$telegram"><img src="vtlgrm.png" class="clicks"></a>\n				<a href="https://m.me/+237$messenger"><img src="vmsg.png" class="clicks"></a>\n			</button>\n			<div class="btn">\n				<img src="click.png" class="click">\n			</div>\n		</div>\n	</div>\n<span class="boutonbas"><a href="#"><img src="haut.png"></a></span>\n\n""")
 write(pages, "$head\n")
-liste = """<ul class="ligne">\n"""
+liste = """<ul class="ligne" style="background: url('fond.png');>\n"""
 somme = 0        
 for i in 1:nbre_rub
     global liste = string(liste, """    <li class="intitule">\n    <ul><button class="rubriques"><a href="index.html#rub-$i">$(rubrique[i])</a></button></ul>\n    <ul>\n""")
@@ -133,6 +132,7 @@ for i in 1:nbre_rub
 end
 liste = string(liste, """</ul>\n\n""")
 write(pages, "$liste\n")
+write(pages, """<div class="titre un">$titre_d_accueil</div>\n    <br><br>\n""")
 write(pages, """<div class="une">A LA UNE</div>\n    <br><br>\n""")
 for i in 1:length(namess)
     write(pages, """    <button class="bouton">\n    <a href="art ($i).png"><img class="produit" src="art ($i).png"></a>\n        <div>\n            <table><tr>\n                <td><div class="haut">$(precisions[i])</div></td><td><div class="hauteurs">$(prices[i]) fcfa</div></td></tr>\n            </table>\n        </div>\n        <div class="hauteur">$(namess[i])</div>\n        <hr>\n</button>\n\n""")
@@ -162,7 +162,7 @@ for i in 1:nbre_rub
     global point = 1
     global somme += nbre_sous_rubrique[i]
 end
-foot = """<span id="basdepage"></span>\n<footer class="fixe full">\n   \n    <div id="liens">\n        <button class="rubrics">\n            <a href="mailto:$email"><img class="contact" src="vmail.png"></a>\n            <a href="https://wa.me/+237$whatsapp/"><img class="contact" src="whtsp.png"></a>\n            <a href="https://t.me/+237$telegram/"><img class="contact" src="vtlgrm.png"></a>\n            <a href="https://m.me/+237$messenger/"><img class="contact" src="vmsg.png"></a>\n        </button>\n        <button class="rubricss">\n            <img class="contacts" src="viaa.png">\n            <img class="contacts" src="vendeur.png">\n            <img class="contacts" src="vshop.png">\n        </button>\n        <button class="rubricsss">\n            <a href="$playlink"><img class="apps" src="vplay.png"></a>\n            <a href="$applink"><img class="apps" src="vapp.png"></a>\n        </button>\n    </div>\n</footer>  \n\n    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>\n    <script src="search.js"></script>                \n</body>\n</html>"""
+foot = """<span id="basdepage"></span>\n<footer class="fixe full">\n   \n    <div id="liens">\n        <button class="rubrics">\n            <a href="mailto:$email"><img class="contact" src="vmail.png"></a>\n            <a href="https://wa.me/+237$whatsapp/"><img class="contact" src="whtsp.png"></a>\n            <a href="https://t.me/+237$telegram/"><img class="contact" src="vtlgrm.png"></a>\n            <a href="https://m.me/+237$messenger/"><img class="contact" src="vmsg.png"></a>\n        </button>\n        <button class="rubricss">\n            <a href="https://viaa.me/"><img class="contacts" src="viaa.png"></a>\n            <img class="contacts" src="vendeur.png">\n            <a href="https://viaa.me/"><img class="contacts" src="vshop.png"></a>\n        </button>\n        <button class="rubricsss">\n            <a href="$playlink"><img class="apps" src="vplay.png"></a>\n            <a href="$applink"><img class="apps" src="vapp.png"></a>\n        </button>\n    </div>\n</footer>  \n\n    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>\n    <script src="search.js"></script>                \n</body>\n</html>"""
 write(pages, "$foot\n")
 close(pages)
 
